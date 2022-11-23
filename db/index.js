cacb0e1636589f179fb172fb87cee30dcb461c0d
Raebaby <78573429+Raebaby@users.noreply.github.com>
2022-11-23 16:43:27 -0600

    Move into node section of part one completed.

--- db/index.js
+++ db/index.js
@@ -5,3 +5,18 @@ const client = new Client('postgres://localhost:5432/juicebox-dev'); //location 
 module.exports = {
     client,
 }
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+  
+  // and export them
+  module.exports = {
+    client,
+    getAllUsers,
+  }
