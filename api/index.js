bfbc06cf80e91388d0b818f25a5953523ba97647
Raebaby <78573429+Raebaby@users.noreply.github.com>
2022-12-22 14:29:39 -0600

    Protecting Our Secrets section of Server pt.2

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require('express'); //importing node_modules
+
+
+//API ROUTES REQUIRE/IMPORTS
 const usersRouter = require('./users'); //importing our usersRouter function from users.js
 const postsRouter = require('./post');
 const tagsRouter = require('./tags');
 const apiRouter = express.Router(); //creating a new router object for api 
 
-
+//API ROUTES - Connect apirouter to the middle ware in our other pages
 apiRouter.use('/users', usersRouter);// invokes a function that starts your middleware for the api router
 apiRouter.use('/posts', postsRouter);// invokes a function that starts your posts middleware for the api router
 apiRouter.use('/tags', tagsRouter);//Allows us to use the middleware functions written in api/tags
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const PORT = 3000; 
 const express = require('express'); // Requires the Express module and puts it in a variable.
 const server = express(); // gives our express function and stores it in a variable
 const apiRouter = require('./api'); // importing apiRouter function 
 const morgan = require('morgan'); //Import our morgan package/dependency that we installed 
-
 
 server.use(morgan('dev')); // sets up of middleware functions with the ablility to use out morgan dependency 
 
